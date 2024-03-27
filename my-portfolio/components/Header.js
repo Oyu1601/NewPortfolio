70c8aeda36e138e36dfc54dda29120c1e9a22a0b
Oyu <lkhagvaoyunerdene743@gmail.com>
2024-03-27 19:39:59 +0800

    first

---
+++ my-portfolio/components/Header.js
@@ -0,0 +1,24 @@
+import Image from "next/image";
+
+import Link from "next/link";
+import Socials from '../components/Socials';
+
+
+const Header = () => {
+  return (
+    <header className="absolute z-30 w-full flex items-center px-16 xl:px-0 xl:h-[90px] ">
+
+      <div className="container mx-auto">
+        <div>
+          {/* <Link >
+            <Image src={'/logo.svg'} width={220} height={48}/>
+          </Link> */}
+        </div>
+      </div>
+    
+    </header>
+  )
+   
+};
+
+export default Header;
